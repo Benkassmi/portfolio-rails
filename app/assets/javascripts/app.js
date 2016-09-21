3b804cadadc2c790704863c8d306060faccfa5db
codecademy <ccuser@codecademy.com>
2016-09-21 05:32:19 -0400

    Portfolio page up and linked

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,3 +1,14 @@
-$(function() {
-    $('.slider').unslider();
-});
+var ready;
+ready = function() {
+    $('.slider').unslider({
+      speed : 500,                //  The speed to animate each slide (in milliseconds)
+			delay : 3000,               //  The delay between slide animations (in milliseconds)
+			complete : function() {},   //  A function that gets called after every slide animation
+			keys : true,                //  Enable keyboard (left, right) arrow shortcuts
+			dots : true,                //  Display dot navigation
+    });
+  $('.slider').height('initial');
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
